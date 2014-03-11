412aa16b91c9b85fc780b15e67789a3fe2506b97
Łukasz Stelmach <stlman@poczta.fm>
2014-03-11 12:47:17 +0100

    First post and Emacs configuration

---
+++ .dir-locals.el
@@ -0,0 +1,20 @@
+((nil . ((indent-tabs-mode . nil)
+	 (tab-width . 4)))
+ (html-mode . ((sgml-basic-offset . 4)))
+ (org-mode . ((org-publish-project-alist
+	       . (("github-blog"
+		   :base-directory "~/proj/eventorbot.blog/org"
+		   :base-extension "org"
+		   :publishing-directory "~/proj/eventorbot.blog"
+		   :recursive t
+		   :publishing-function org-html-publish-to-html
+		   :headline-level 4
+		   :body-only t ;; Only export section between <body> </body>
+		   )
+		  ("github-static"
+		   :base-directory "~/proj/eventorbot.blog/org"
+		   :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|stl"
+		   :publishing-directory "~/proj/eventorbot.blog/"
+		   :recursive t
+		   :publishing-function org-publish-attachment)
+		  ("github" :components ("github-blog" "github-static")))))))
